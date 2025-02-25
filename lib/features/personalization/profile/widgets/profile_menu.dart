8363f007d18be3cb6ae7d12db863c1fd605a5d06
karim <karimslama917@gmail.com>
2025-02-25 19:59:01 +0200

    - Designed Profile Screen with an intuitive & clean UI - Enabled profile picture update functionality. - Added options to edit name, username, email, and phone number. - Implemented copy user ID feature for easy access. - Provided options to update gender & date of birth. - Included 'close account' option for account management. - Ensured full Light & Dark theme support. - Optimized UI for better user experience & accessibility.

---
+++ lib/features/personalization/profile/widgets/profile_menu.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:qaf_store/utils/constants/qaf_sizes.dart';
+
+class ProfileMenu extends StatelessWidget {
+  final String title, value;
+  final VoidCallback onTap;
+  final IconData icon;
+  const ProfileMenu({
+    super.key,
+    required this.title,
+    required this.value,
+    required this.onTap,
+    this.icon = Iconsax.arrow_right_34,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: onTap,
+      child: Padding(
+        padding: EdgeInsetsDirectional.symmetric(
+          vertical: QafSizes.spaceBtwItems / 1.5,
+        ),
+        child: Row(
+          children: [
+            Expanded(
+              flex: 3,
+              child: Text(
+                title,
+                style: Theme.of(context).textTheme.bodySmall,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Expanded(
+              flex: 5,
+              child: Text(
+                value,
+                style: Theme.of(context).textTheme.bodyMedium,
+                overflow: TextOverflow.ellipsis,
+              ),
+            ),
+            Expanded(child: Icon(icon, size: 18)),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- lib/navigation_menu.dart
+++ lib/navigation_menu.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 import 'package:iconsax/iconsax.dart';
 import 'package:qaf_store/features/store/screens/favorite/wishlist_screen.dart';
 import 'package:qaf_store/features/store/screens/home/home_screens.dart';
-import 'package:qaf_store/features/personalization/profile/settings_screen.dart';
+import 'package:qaf_store/features/personalization/settings/settings_screen.dart';
 import 'package:qaf_store/features/store/screens/store/store_screen.dart';
 import 'package:qaf_store/utils/constants/qaf_colors.dart';
 import 'package:qaf_store/utils/constants/qaf_strings.dart';
