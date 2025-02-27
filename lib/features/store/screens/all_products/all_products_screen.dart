00ae7f882eb421fdb032a8f9014e3dfaaaf48afb
karim <karimslama917@gmail.com>
2025-02-27 14:56:09 +0200

    - Designed All Products Screen with dynamic filtering options:   - Sort by Name, Price (High/Low), Sale, Newest, Popularity. - Reused GridView component for efficient product listing. - Integrated Vertical Product Card for consistent UI/UX. - Ensured smooth & responsive design across devices. - Optimized for Light & Dark Mode compatibility.

---
+++ lib/features/store/screens/all_products/all_products_screen.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+import 'package:qaf_store/common/widgets/appbar/appbar.dart';
+import 'package:qaf_store/common/widgets/custom_shapes/containers/qaf_search_container.dart';
+import 'package:qaf_store/common/widgets/layout/grid_view_layout.dart';
+import 'package:qaf_store/common/widgets/products/product_cards/vertical_product_card.dart';
+import 'package:qaf_store/utils/constants/qaf_sizes.dart';
+import 'package:qaf_store/utils/constants/qaf_strings.dart';
+
+class AllProductsScreen extends StatelessWidget {
+  const AllProductsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: QafAppBar(
+        title: Text('Popular Products',
+            style: Theme.of(context).textTheme.headlineSmall),
+        showBackArrow: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: EdgeInsetsDirectional.all(QafSizes.defaultSpace),
+          child: Column(
+            spacing: QafSizes.spaceBtwSections,
+            children: [
+              DropdownButtonFormField(
+                decoration: InputDecoration(prefixIcon: Icon(Iconsax.sort)),
+                items: [
+                  'Name',
+                  'Higher Price',
+                  'Lower Price',
+                  'Sale',
+                  'Newest',
+                  'Popularity'
+                ]
+                    .map((option) =>
+                        DropdownMenuItem(value: option, child: Text(option)))
+                    .toList(),
+                onChanged: (value) {},
+              ),
+              GridViewLayout(
+                itemCount: 16,
+                itemBuilder: (_, index) => VerticalProductCard(),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
